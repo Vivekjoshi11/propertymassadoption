6d5ad2f518dcf9e275fe8821719bea6c1be10298
Vivek Joshi <108278744+Vivekjoshi11@users.noreply.github.com>
2024-12-31 13:05:59 +0530

    Update 2-addPropertiesToDb.ts

--- src/scripts/2-addPropertiesToDb.ts
+++ src/scripts/2-addPropertiesToDb.ts
@@ -118,6 +118,7 @@ async function addProperties() {
             longitude: property.longitude,
             latitude: property.latitude,
         }))
+        console.log(formattedResult)
 
         // Write the result to a file in the desired format
         writeFileSync(
